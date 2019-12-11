3ba9b171d3cf38eecd19f417d8da5560b7660605
Leo Zhuxing Li <44008970+LeoZhuxingLi@users.noreply.github.com>
2019-12-11 18:34:26 -0500

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,181 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+"title" => "The Hunger Games",
+"img" => "img/media/hungergames.png",
+"genre" => "Dystopian",
+"format" => "Hardcover",
+"year" => 2008,
+"category" => "Books",
+"authors" => [
+    "Suzanne Collins"
+],
+"publisher" => "Scholastic Press",
+"isbn" => '9780439023528'
+];
+$catalog[102] = [
+"title" => "The Spiderwick Chronicles: The Field Guide",
+"img" => "img/media/spiderwick.png",
+"genre" => "Children's Literature",
+"format" => "Hardcover",
+"year" => 2003,
+"category" => "Books",
+"authors" => [
+    "Holly Black",
+],
+"publisher" => "Simon & Schuster",
+"isbn" => '9781442486928'
+];
+$catalog[103] = [
+"title" => "A Series of Unfortunate Events: The Bad Beginning",
+"img" => "img/media/seriesofunfort.png",
+"genre" => "Gothic Fiction",
+"format" => "Hardcover",
+"year" => 1999,
+"category" => "Books",
+"authors" => [
+    "Lemony Snicket"
+],
+"publisher" => "HarperCollins Publishers",
+"isbn" => '9780064407663'
+];
+$catalog[104] = [
+"title" => "Percy Jackson and the Olympians Series: The Lightning Thief",
+"img" => "img/media/percyjackson.png",
+"genre" => "Greek Mythology",
+"format" => "Paperback",
+"year" => 2005,
+"category" => "Books",
+"authors" => [
+    "Rick Riordan"
+],
+"publisher" => "Disney Press",
+"isbn" => '9780786838653'
+];
+//Movies
+$catalog[201] = [
+"title" => "2012",
+"img" => "img/media/2012.png",
+"genre" => "Disaster",
+"format" => "DVD/Blu-ray",
+"year" => 2009,
+"category" => "Movies",
+"director" => "Roland Emmerich",
+"writers" => [
+    "Harald Kloser",
+    "Roland Emmerich"
+],
+"stars" => [
+    "John Cusack",
+    "Chiwetel Ejiofor",
+    "Amanda Peet",
+    "Oliver Platt",
+    "Thandie Newton",
+    "Danny Glover",
+    "Woody Harrelson"
+]
+];
+$catalog[202] = [
+"title" => "12 years a slave",
+"img" => "img/media/12years.png",
+"genre" => "Biography, Drama, History",
+"format" => "DVD/Blu-ray",
+"year" => 2013,
+"category" => "Movies",
+"director" => "Steve McQueen",
+"writers" => [
+    "John Ridley",
+    "Solomon Northup"
+],
+"stars" => [
+    "Chiwetel Ejiofor",
+    "Michael Fassbender",
+    "Benedict Cumberbatch",
+    "Michael Kenneth Williams",
+    "Lupita Nyong'o",
+    "Brad Pitt"
+]
+];
+$catalog[203] = [
+"title" => "Spider-Man: Into the Spider-Verse",
+"img" => "img/media/intothespiderverse.png",
+"genre" => "Animation",
+"format" => "DVD/Blue-ray",
+"year" => 2018,
+"category" => "Movies",
+"director" => "Bob Persichetti",
+"writers" => [
+    "Phil Lord",
+    "Rodney Rothman"
+],
+"stars" => [
+    "Shameik Moore",
+    "Jake Johnson",
+    "Hailee Steinfeld",
+    "Mahershala Ali",
+    "Kimiko Glenn",
+    "Nicolas Cage",
+    "Liev Schreiber"
+]
+];
+$catalog[204] = [
+"title" => "Logan",
+"img" => "img/media/logan.png",
+"genre" => "Sci-Fi",
+"format" => "DVD/Blu-ray",
+"year" => 2017,
+"category" => "Movies",
+"director" => "James Mangold",
+"writers" => [
+    "James Mangold",
+    "Scott Frank",
+    "Michael Green"
+],
+"stars" => [
+    "Hugh Jackman",
+    "Patrick Stewart",
+    "Richard E. Grant",
+    "Boyd Holbrook",
+    "Stephan Mercharnt",
+    "Dafne Keen"
+]
+];
+//Music
+$catalog[301] = [
+"title" => "Adventure",
+"img" => "img/media/madeon.jpg",
+"genre" => "Electropop, House, Nu-Disco",
+"format" => "CD",
+"year" => 2015,
+"category" => "Music",
+"artist" => "Madeon"
+];
+$catalog[302] = [
+"title" => "A Head Full of Dreams",
+"img" => "img/media/coldplay.jpeg",
+"genre" => "Pop, Pop Rock, Alternative Rock",
+"format" => "CD/Vinyl",
+"year" => 2015,
+"category" => "Music",
+"artist" => "Coldplay"
+];
+$catalog[303] = [
+"title" => "Clarity",
+"img" => "img/media/zedd.jpg",
+"genre" => "EDM, Electro House, Progressive House, Dance-Pop",
+"format" => "CD/Vinyl",
+"year" => 2012,
+"category" => "Music",
+"artist" => "Zedd"
+];
+$catalog[304] = [
+"title" => "Beerbongs & Bentleys",
+"img" => "img/media/postmalone.jpg",
+"genre" => "Pop, Hip-Hop, R&B",
+"format" => "CD/Vinyl",
+"year" => 2018,
+"category" => "Music",
+"artist" => "Post Malone"
+];
+?>
